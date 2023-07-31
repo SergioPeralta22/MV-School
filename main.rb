7476c8e11ed5dcaa69dc9fe15d14d7d37df11b51
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-07-31 14:28:54 -0300

    Apply YAGNI, KISS, DRY and SOLID on main.rb

--- main.rb
+++ main.rb
@@ -1,37 +1,8 @@
-require_relative 'app'
-require_relative 'print_options'
+require_relative 'manage_option'
 
 def main
-  app = App.new
-  print_options = PrintOptions.new
-
-  loop do
-    print_options.show
-    option = gets.chomp.to_i
-
-    if option == 7
-      puts 'Thank you for using this app!'
-      break
-    elsif option.between?(1, 6)
-      case option
-      when 1
-        app.list_books
-      when 2
-        app.list_people
-      when 3
-        app.create_person
-      when 4
-        app.create_book
-      when 5
-        app.create_rental
-      when 6
-        app.list_rentals
-      end
-    else
-      puts 'Error: Invalid number, try again'
-    end
-  end
+  manage_option = ManageOption.new
+  manage_option.manage_option
 end
-
 
 main
