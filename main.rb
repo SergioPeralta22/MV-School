0a484ac27d41f5454c05627fd42f211b67a6b268
Sergio Peralta <seperalta88@gmail.com>
2023-07-31 13:15:33 -0300

    Create PrintOptions class

--- main.rb
+++ main.rb
@@ -1,22 +1,12 @@
 require_relative 'app'
-
-def print_options
-  puts ''
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
+require_relative 'print_options'
 
 def main
   app = App.new
+  print_options = PrintOptions.new
 
   loop do
-    print_options
+    print_options.show
     option = gets.chomp.to_i
 
     if option == 7
@@ -42,5 +32,6 @@ def main
     end
   end
 end
+
 
 main
