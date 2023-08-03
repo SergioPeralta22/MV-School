b51f7b4ddd0c2f5d9220c1ca849f2ce09e680ee5
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-03 10:24:50 -0300

    Fix Linters

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -9,7 +9,7 @@ describe Decorator do
 
   context 'correct_name' do
     it 'should capitalize the name' do
-      print @capitalized_person.correct_name 
+      print @capitalized_person.correct_name
       expect(@capitalized_person.correct_name).to eq 'Maximilianus'
     end
   end
